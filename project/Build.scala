77fde0b2aed8bd8382b210a98e0638cba812dde3
Michael Bayne <mdb@samskivert.com>
2013-04-16 12:31:04 -0700

    Prepare for Maven Central release with com.samskivert group id.
    I've pretty much coopted the project, so we'll finish the job.

--- project/Build.scala
+++ project/Build.scala
@@ -4,24 +4,48 @@ import Keys._
 object HighchairBuild extends Build {
 
   val highSettings = Defaults.defaultSettings ++ seq(
-    organization := "net.thegodcode",
+    organization := "com.samskivert",
     name := "Highchair",
-    version := "0.0.5-SNAPSHOT",
+    description := "A set of modules for developing Google App Engine services/apps in Scala.",
+    version := "0.0.5",
     scalaVersion := "2.10.0",
     crossScalaVersions := Seq("2.9.2"),
     scalacOptions += "-deprecation",
     libraryDependencies ++= GAE.dependencies ++ Seq(
       "org.specs2" %% "specs2" % "1.13" % "test"),
     parallelExecution in Test := false,
-    resolvers := Seq(ScalaToolsSnapshots),
-    publishTo <<= (version) { version: String =>
-      val nexus = "http://nexus.scala-tools.org/content/repositories/"
-      if (version.trim.endsWith("SNAPSHOT"))
-        Some("snapshots" at nexus+"snapshots/")
-      else
-        Some("releases" at nexus+"releases/")
+
+    // bits for publishing to Maven Central
+    publishMavenStyle := true,
+    // don't add the scala-tools repository to our POM
+    pomIncludeRepository := { (repo: MavenRepository) => false },
+    publishTo <<= (version) { v: String =>
+      val root = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at root + "content/repositories/snapshots/")
+      else Some("staging" at root + "service/local/staging/deploy/maven2")
     },
-    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+    credentials += Credentials(Path.userHome / ".ivy2" / "credentials-sonatype"),
+    homepage := Some(url("http://github.com/samskivert/highchair")),
+    licenses += ("The (New) BSD License" ->
+      url("http://www.opensource.org/licenses/bsd-license.php")),
+    startYear := Some(2010),
+    pomExtra :=
+      <developers>
+        <developer>
+          <id>chrislewis</id>
+          <name>Chris Lewis</name>
+          <email>highchair-user@googlegroups.com</email>
+        </developer>
+        <developer>
+          <id>samskivert</id>
+          <name>Michael Bayne</name>
+          <email>mdb@samskivert.com</email>
+        </developer>
+      </developers>
+      <scm>
+        <connection>scm:git:git://github.com/samskivert/highchair.git</connection>
+        <url>http://github.com/samskivert/highchair</url>
+      </scm>
   )
 
   lazy val root = Project("highchair", file("."), settings = highSettings).aggregate(
@@ -31,7 +55,9 @@ object HighchairBuild extends Build {
     libraryDependencies += "joda-time" % "joda-time" % "1.6.2"
   )) dependsOn(spec % "test")
   lazy val spec = Project("highchair-spec", file("spec"), settings = highSettings ++ seq(
-    name := "Highchair Spec"
+    name := "Highchair Spec",
+    publishLocal := false,
+    publish := false
   ))
   lazy val remote = Project("highchair-remote", file("remote"), settings = highSettings ++ seq(
     name := "Highchair Remote"
